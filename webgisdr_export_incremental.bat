337c11f0fefd4279f7ba327bb7c4491e25ea0064
Dan Giersz <90721544+dgierszcc@users.noreply.github.com>
2024-05-24 14:53:38 -0400

    Batch file called from script to run webgisdr

---
+++ webgisdr_export_incremental.bat
@@ -0,0 +1,7 @@
+c:
+
+cd "C:\Program Files\ArcGIS\Portal\tools\webgisdr"
+
+webgisdr --export --file "C:\Program Files\ArcGIS\Portal\tools\webgisdr\webgisdr-Incremental.properties" --output "C:\Program Files\ArcGIS\Portal\tools\webgisdr\webgisdrlogoutput.json"
+
+pause
